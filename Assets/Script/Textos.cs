97a007cee0472daf7cadbaa50f772b7e2c07505e
Breno E S L Reis <brereis@outlook.com>
2023-04-19 17:29:23 -0300

    19/04/2023 (17:30)

--- Assets/Script/Textos.cs
+++ Assets/Script/Textos.cs
@@ -8,13 +8,14 @@ public class Textos : MonoBehaviour
     [SerializeField] string _Nome = "Tijuca";
     [SerializeField] string _Sobrenome;
     [SerializeField] string _NomeCompleto;
-    
-    
+    [SerializeField] Text _texto;
+        
     // Start is called before the first frame update
     void Start()
     {
         _Nome = "Jorgeh";
         _NomeCompleto = _Nome + " " + _Sobrenome;
+        _texto.text = _NomeCompleto;
     }
 
     // Update is called once per frame
