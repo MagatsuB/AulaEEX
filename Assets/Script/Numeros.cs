30b047e01c0ea71caea09438d9c5daba8dce10a1
Breno E S L Reis <brereis@outlook.com>
2023-04-24 17:57:44 -0300

    Aula(24.04.2023)
    Aula de terça 24 de abril 2023

--- Assets/Script/Numeros.cs
+++ Assets/Script/Numeros.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Numeros : MonoBehaviour
 {
+    [SerializeField] int _strgravity;
+    [SerializeField] int _weakgravity;
+    [SerializeField] float _gravidade;
+    [SerializeField] GameObject _ball;
+    [SerializeField] Rigidbody _rig;
+    [SerializeField] Text _tex;
+
     // Start is called before the first frame update
     void Start()
     {
+        //_rig.useGravity = true;
+        _gravidade = _gravidade * 3;
+        _tex.text = "Gravidade =  " + _gravidade;
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        _rig.mass = _gravidade ;
         
+
     }
 }
