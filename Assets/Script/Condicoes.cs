6f3d56dc19740b2c67797083a638988dc4941e93
Breno E S L Reis <brereis@outlook.com>
2023-04-28 16:00:34 -0300

    Update 2804.2023
    Aula de sexta 28.05.2023

--- Assets/Script/Condicoes.cs
+++ Assets/Script/Condicoes.cs
@@ -15,26 +15,55 @@ public class Condicoes : MonoBehaviour
     // >= Maior ou igual
     // != Diferente
     // == igual
-
+    // && e
+    // || OU
 
     // Start is called before the first frame update
     void Start()
     {
+        //_i1 = 1;
         //if (_i1 == 1) {
-        //    Debug.Log("O valor de i1 é: " + _i1);
+        //    Debug.Log("fase 1 liberada ");
         //}
         //else
         //{
-        //    Debug.Log("O Valor de i1 não é 1");
+        //    Debug.Log("Fase 1 bloqueada ");
         //}
 
+        // se t1 for igual a multiplicação 
+        //multiplicar i1 com a i2 
+        // se nao somar i1 com i2
+
+        //_i1 = 2; _i2 = 4;
+        //
+        //if(_t1 == "multiplicacao")
+        //{
+        //    int _mult = _i1 * _i2;
+        //    Debug.Log("A Multiplicação entre i1 e i2 é " + _mult);
+        //}
+        //else
+        //{
+        //    int _soma = _i1 + _i2;
+        //    Debug.Log("A soma entre i1 e i2 é " + _soma);
+        //}
+
+
+        //Se a _i1 for igual a 2
+        //a _t1 escreve passagem liberada
+        //se não _t1, passagem bloqueada
+
         _i1 = 1;
-        _i2 = 1;
-           if(_i1 == _i2)
+        if(_i1 == 2)
         {
-            Debug.Log("Valor de i1 é igual a i2");
+            _t1 = "Passagem Liberada";
         }
-        
+        else
+        {
+            _t1 = "Pasagem Bloqueada";
+        }
+
+        Debug.Log(_t1);
+
     }
 
     // Update is called once per frame
